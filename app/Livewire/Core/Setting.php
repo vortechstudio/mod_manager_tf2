1e1c9ad683a9ca288119bf66688324a9e2935ac8
Maxime Mockelyn <contact@vortechstudio.fr>
2024-10-16 17:18:17 +0200

    Sauvegarde les configurations dans un fichier JSON

--- app/Livewire/Core/Setting.php
+++ app/Livewire/Core/Setting.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Core;
 
 use App\Models\Config;
+use Illuminate\Support\Facades\File;
 use Livewire\Component;
 
 class Setting extends Component
@@ -13,8 +14,9 @@ class Setting extends Component
     public function mount()
     {
         try {
-            $this->tempPath = Config::where('name', 'temp_path')->firstOrFail()->value;
-            $this->stagingPath = Config::where('name', 'staging_path')->firstOrFail()->value;
+            $config = $this->readConfig();
+            $this->tempPath = $config['temp_path'];
+            $this->stagingPath = $config['staging_path'];
         } catch (\Exception $e) {
             flash()->error($e->getMessage());
         }
@@ -23,23 +25,36 @@ class Setting extends Component
     public function save()
     {
         try {
-            $tempConfig = Config::where('name', 'temp_path')->first();
-            $stagingConfig = Config::where('name', 'staging_path')->first();
+            $config = [
+                'temp_path' => $this->tempPath,
+                'staging_path' => $this->stagingPath,
+            ];
 
-            if ($tempConfig && $stagingConfig) {
-                $tempConfig->value = $this->tempPath;
-                $tempConfig->save();
-
-                $stagingConfig->value = $this->stagingPath;
-                $stagingConfig->save();
-
-                flash()->addSuccess("Configuration Terminer !");
-            } else {
-                throw new \Exception('Error system');
-            }
-        } catch (\Exception $exception) {
-            flash()->addError($exception->getMessage());
+            $this->writeConfig($config);
+            flash()->success('Configuration sauvegardée avec succès');
+        }catch (\Exception $e) {
+            flash()->error($e->getMessage());
         }
+    }
+
+    private function readConfig()
+    {
+        $configPath = base_path('config/config.json');
+        if (File::exists($configPath)) {
+            return json_decode(File::get($configPath), true);
+        }
+
+        // Return empty values if config file does not exist
+        return [
+            'temp_path' => '',
+            'staging_path' => '',
+        ];
+    }
+
+    private function writeConfig(array $config)
+    {
+        $configPath = base_path('config/config.json');
+        File::put($configPath, json_encode($config, JSON_PRETTY_PRINT));
     }
 
     public function render()
