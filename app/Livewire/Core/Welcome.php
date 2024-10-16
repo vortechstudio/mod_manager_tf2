68d4cfbf84e43449a8a923cec69870bbc5802e39
Maxime Mockelyn <contact@vortechstudio.fr>
2024-10-16 22:17:04 +0200

    Ajoute des fonctionnalités de gestion des mods et un guide rapide dans l'interface utilisateur

--- app/Livewire/Core/Welcome.php
+++ app/Livewire/Core/Welcome.php
@@ -2,10 +2,64 @@
 
 namespace App\Livewire\Core;
 
+use App\Services\SteamWorkshop;
+use Illuminate\Support\Facades\File;
 use Livewire\Component;
 
 class Welcome extends Component
 {
+    public $modStats = [];
+
+    public function mount()
+    {
+        $this->modStats['created'] = $this->getModCount('created');
+        $this->modStats['modified'] = $this->getModCount('modified');
+        $this->modStats['validated'] = $this->getModCount('validated');
+        $this->modStats['total'] = $this->getModCount('total');
+    }
+
+    private function readConfig()
+    {
+        $configPath = base_path('config/config.json');
+        if (File::exists($configPath)) {
+            return json_decode(File::get($configPath), true);
+        }
+
+        // Return empty values if config file does not exist
+        return [
+            'temp_path' => '',
+            'staging_path' => '',
+        ];
+    }
+
+    public function getModCount(string $symbol)
+    {
+        $config = $this->readConfig();
+        $stagingPath = $config['staging_path'];
+        $tempPath = $config['temp_path'];
+        switch ($symbol) {
+            case 'created':
+                return File::directories($tempPath) ? count(File::directories($tempPath)) : 0;
+            case 'modified':
+                $directories = File::directories($tempPath);
+                $modifiedMod = 0;
+
+                foreach ($directories as $directory) {
+                    if (File::exists($directory . DIRECTORY_SEPARATOR . 'version.txt')) {
+                        $modifiedMod++;
+                    }
+                }
+                return $modifiedMod;
+
+            case 'validated':
+                return File::directories($stagingPath) ? count(File::directories($stagingPath)) : 0;
+
+            case 'total':
+                return 0;
+
+        }
+    }
+
     public function render()
     {
         return view('livewire.core.welcome');
--- resources/views/livewire/core/welcome.blade.php
+++ resources/views/livewire/core/welcome.blade.php
@@ -1,31 +1,49 @@
-<div class="card m-3" style="width: 100%">
-    <div class="card-body">
-        <h1 class="card-title">Bienvenue sur le Mod Manager TF2</h1>
-
-        <p>Gérez facilement vos mods pour Transport Fever 2, configurez vos dossiers, et vérifiez vos mods pour les erreurs en quelques clics.</p>
-        <div class="features">
-            <h3>Fonctionnalités principales :</h3>
-            <ul>
-                <li>Gestion des dossiers : Configurez les chemins des dossiers utilisés pour le staging et les fichiers temporaires.</li>
-                <li>Vérification des mods : Vérifiez la validité des mods avant de les utiliser dans le jeu.</li>
-                <li>Notifications automatiques : Recevez des alertes en cas d'erreur ou de mise à jour disponible.</li>
-            </ul>
-        </div>
-        <!-- Section du Guide rapide -->
-        <div class="card mt-4">
-            <div class="card-header">
-                <h5>Guide rapide</h5>
+<div class="container-fluid mt-1" style="width: 100%">
+    <div class="row mb-3">
+        <div class="col">
+            <div class="card">
+                <div class="card-body bg-color-blue-500 text-white rounded-2">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span class="fs-5">Nombre de mod créer</span>
+                        <span class="fs-1">5</span>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body bg-color-yellow-500 text-black-50 rounded-2">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span class="fs-5">Nombre de mod modifier</span>
+                        <span class="fs-1">1</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body bg-color-green-500 text-white rounded-2">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span class="fs-5">Nombre de mod Valider</span>
+                        <span class="fs-1">1</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Bienvenue sur le Mod Manager TF2</h1>
+
+            <p>Gérez facilement vos mods pour Transport Fever 2, configurez vos dossiers, et vérifiez vos mods pour les erreurs en quelques clics.</p>
+            <div class="features">
+                <h3>Fonctionnalités principales :</h3>
                 <ul>
-                    <li><strong>Configurer les dossiers</strong> : Allez dans la section <em>Configuration</em> pour définir les chemins des dossiers temporaires et de staging.</li>
-                    <li><strong>Créer un mod</strong> : Utilisez l'option <em>Nouveau Mod</em> pour créer un nouveau mod avec les dossiers et fichiers nécessaires.</li>
-                    <li><strong>Convertir des images</strong> : Le <em>Convertisseur TGA/DDS</em> vous permet de convertir facilement vos fichiers images pour Transport Fever 2.</li>
-                    <li><strong>Vérifier les mods</strong> : Avant de publier un mod, utilisez l'outil <em>Vérificateurs</em> pour vérifier la validité des fichiers et s'assurer qu'il n'y a pas d'erreurs.</li>
-                    <li><strong>Notifications</strong> : Surveillez les notifications qui vous alertent en cas d'erreurs ou de mises à jour importantes.</li>
+                    <li>Gestion des dossiers : Configurez les chemins des dossiers utilisés pour le staging et les fichiers temporaires.</li>
+                    <li>Vérification des mods : Vérifiez la validité des mods avant de les utiliser dans le jeu.</li>
+                    <li>Notifications automatiques : Recevez des alertes en cas d'erreur ou de mise à jour disponible.</li>
                 </ul>
             </div>
         </div>
-        <!-- Fin du guide rapide -->
     </div>
 </div>
