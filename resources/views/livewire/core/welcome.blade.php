08b244b742b52ef138744196aba77e140947be2a
Maxime Mockelyn <contact@vortechstudio.fr>
2024-10-16 22:19:15 +0200

    Ajoute un fichier version.txt et met à jour les statistiques de mod dans la vue

--- resources/views/livewire/core/welcome.blade.php
+++ resources/views/livewire/core/welcome.blade.php
@@ -5,7 +5,7 @@
                 <div class="card-body bg-color-blue-500 text-white rounded-2">
                     <div class="d-flex justify-content-between align-items-center">
                         <span class="fs-5">Nombre de mod créer</span>
-                        <span class="fs-1">5</span>
+                        <span class="fs-1">{{ $modStats['created'] }}</span>
                     </div>
                 </div>
             </div>
@@ -15,7 +15,7 @@
                 <div class="card-body bg-color-yellow-500 text-black-50 rounded-2">
                     <div class="d-flex justify-content-between align-items-center">
                         <span class="fs-5">Nombre de mod modifier</span>
-                        <span class="fs-1">1</span>
+                        <span class="fs-1">{{ $modStats['modified'] }}</span>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
                 <div class="card-body bg-color-green-500 text-white rounded-2">
                     <div class="d-flex justify-content-between align-items-center">
                         <span class="fs-5">Nombre de mod Valider</span>
-                        <span class="fs-1">1</span>
+                        <span class="fs-1">{{ $modStats['validated'] }}</span>
                     </div>
                 </div>
             </div>
